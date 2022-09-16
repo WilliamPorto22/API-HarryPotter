651f823603d48a90f5d2e6468dc73e91ba324fa4
WilliamPorto22 <williamporto0@gmail.com>
2022-09-16 19:20:38 -0300

    Atualizacao

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- // API Harry Potter    http://hp-api.herokuapp.com/api/characters
+// API Harry Potter    http://hp-api.herokuapp.com/api/characters
 
 
 // este exemplos abaixo mostrata o corpo da API no console 
@@ -9,11 +9,16 @@
 //     console.log(corpo)  
 // })
 
+//  exemplo 2
+
+fetch('http://hp-api.herokuapp.com/api/characters')
+.then(resposta => resposta.json())
+.then(console.log)
 
 // Este exemplos abaixo mostrara o resultado da API no HTML
 
-fetch('http://hp-api.herokuapp.com/api/characters').then(resposta =>{
-    return resposta.json()
-}).then(corpo=>{
-    document.getElementById('span').innerHTML=corpo.name  // Depois do innerhtml vai o =corpo.  e depois vai o que voce quer mostrar na API
-})
+// fetch('http://hp-api.herokuapp.com/api/characters').then(resposta =>{
+//     return resposta.json()
+// }).then(corpo=>{
+//     document.getElementById('span').innerHTML=corpo.name  // Depois do innerhtml vai o =corpo.  e depois vai o que voce quer mostrar na API
+// })
